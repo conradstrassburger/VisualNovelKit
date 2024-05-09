c1d16d04c57f42e809bc5237aa4dd64bf98f7ede
Jeremi Biernacki <jeremi360@gmail.com>
2024-05-09 12:09:40 +0200

    add keep_on_menu option

--- scenes/DialogueUI/SayPanel.gd
+++ scenes/DialogueUI/SayPanel.gd
@@ -1,14 +1,40 @@
 extends DialoguePanel
 
+## if true then it will be shown on `menu` staments
+@export var keep_on_menu := true
+
 func _ready():
 	super._ready()
 	Rakugo.sg_say.connect(set_labels)
+
+	if keep_on_menu:
+		Rakugo.sg_menu.connect(_on_menu)
+
+func _on_say(character:Dictionary, text:String):
+	next_button.show()
+	set_labels(character, text)
 
 func _process(_delta):
 	if !Rakugo.is_waiting_step():
 		hide()
 		return
 
+	if keep_on_menu:
+		if (not Rakugo.is_waiting_step()
+		and not Rakugo.is_waiting_menu_return()):
+			hide()
+			return
+	
+	elif not Rakugo.is_waiting_step():
+			hide()
+			return
+	
+	if not keep_on_menu:
 	if Input.is_action_just_pressed("interact"):
 		Rakugo.do_step()
 		hide()
+
+func _on_menu(_choices:Array):
+	next_button.hide()
+	show()
+
