2958df50878c46808408a6ae72a14d1bd0ebaac7
Jeremi Biernacki <jeremi360@gmail.com>
2024-05-09 10:30:21 +0200

    Add base class for Say and Ask Panels

---
+++ scenes/DialogueUI/DialoguePanel.gd
@@ -0,0 +1,25 @@
+extends Panel
+class_name DialoguePanel
+
+@export var character_name_label : AdvancedTextLabel
+@export var dialogue_label : AdvancedTextLabel
+
+func _ready():
+	visibility_changed.connect(_on_visibility_changed)
+	hide()
+
+func set_labels(character:Dictionary, text:String):
+	if !visible:
+		show()
+	
+	var name_label = "[h1]{text}[/h1]" % character.get("name", "")
+	var character_color = character.get("color", null)
+	
+	if character_color:
+		name_label = "[color=%s]%s[/color]" % name_label
+	
+	character_name_label._text = name_label
+	dialogue_label._text = text
+
+func _on_visibility_changed():
+	set_process(visible)
