fd0484f9c6ffae9c44ac239450d4be68a826fb81
Jeremi Biernacki <jeremi360@gmail.com>
2024-06-02 16:27:37 +0200

    make regex into plugin

--- addons/rakugo_regex_lab/regex.gd
+++ addons/rakugo_regex_lab/regex.gd
@@ -1,10 +1,22 @@
-extends VBoxContainer
+@tool
+extends Window
+
+@export var window: Window
+
+var RkRegex := {
+	NAME = "[a-zA-Z][a-zA-Z_0-9]*",
+	NUMERIC = "-?[0-9]\\.?[0-9]*",
+	STRING = "\".+?\"",
+	VARIABLE = "((?<char_tag>{NAME})\\.)?(?<var_name>{NAME})",
+	ASSIGNMENT = "(?<assignment>=|\\+=|\\-=|\\*=|\\/=)",
+}
 
 var regex = RegEx.new()
 
 func _ready():
 	%Text.set_text("They asked me \"What's going on \\\"in the manor\\\"?\"")
 	update_expression(%Expression.text)
+	close_requested.connect(hide)
 
 func update_expression(text):
 	regex.compile(text)
@@ -15,8 +27,8 @@ func update_text():
 		child.queue_free()
 	
 	if regex.is_valid():
-		$HBoxContainer.modulate = Color.WHITE
-		$HBoxContainer3.modulate = Color.WHITE
+		%RkRegexBox.modulate = Color.WHITE
+		%RegexBox.modulate = Color.WHITE
 		var matches = regex.search_all(%Text.get_text())
 		if matches.size() >= 1:
 			# List all matches and their respective captures.
@@ -36,18 +48,17 @@ func update_text():
 					%List.add_child(capture_label)
 					capture_number += 1
 	else:
-		$HBoxContainer.modulate = Color(1, 0.2, 0.1)
-		$HBoxContainer3.modulate = Color(1, 0.2, 0.1)
+		%RkRegexBox.modulate = Color(1, 0.2, 0.1)
+		%RegexBox.modulate = Color(1, 0.2, 0.1)
 		var label = Label.new()
 		label.text = "Error: Invalid regular expression. Check if the expression is correctly escaped and terminated."
 		%List.add_child(label)
 
-func _on_help_meta_clicked(_meta):
-	# Workaround for clickable link doing nothing when clicked.
-	OS.shell_open("https://regexr.com")
+func _on_help_meta_clicked(meta):
+	OS.shell_open(meta)
 
 func _on_rkreg_expression_text_changed(new_text):
-	new_text = new_text.format(Rakugo.parser.Regex)
+	new_text = new_text.format(RkRegex)
 	%RegExpression.text = new_text
 	regex.compile(new_text)
 	update_text()
