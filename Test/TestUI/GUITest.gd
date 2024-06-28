a427862cd8291b653b979efb27dfbd9640b9430f
Jeremi Biernacki <jeremi360@gmail.com>
2024-06-28 18:51:55 +0200

    now SayUITest is complete

--- Test/TestUI/GUITest.gd
+++ Test/TestUI/GUITest.gd
@@ -3,11 +3,11 @@ class_name GUITest
 
 var timer : Timer
 
-func add_panel_from_scene(path:String) -> DialoguePanel:
+func add_from_scene(path:String) -> Node:
 	var scene = load(path)
-	var panel := scene.instantiate() as DialoguePanel
-	get_tree().current_scene.add_child(panel)
-	return panel
+	var node = scene.instantiate()
+	get_tree().current_scene.add_child(node)
+	return node
 
 func assert_dialogue_panel(dialogue_panel: DialoguePanel):
 	assert_not_null(dialogue_panel)
@@ -18,9 +18,9 @@ func assert_dialogue_panel_text(dialogue_panel: DialoguePanel, character_name, d
 	assert_eq(dialogue_panel.character_name_label._text, character_name)
 	assert_eq(dialogue_panel.dialogue_label._text, dialogue_text)
 
-func assert_visblity(control: Control, visibility := true):
+func wait_visblity(control: Control, visibility := true):
 	await wait_for_signal(control.visibility_changed, 0.2)
 	if visibility:
-		assert_true(control.visible)
+		assert_true(control.visible, control.name + " visibility")
 	else:
-		assert_false(control.visible)
+		assert_false(control.visible, control.name + " visibility")
--- Test/TestUI/TestSayUI.gd
+++ Test/TestUI/TestSayUI.gd
@@ -6,41 +6,32 @@ const file_path = "res://Test/TestUI/TestSay.rk"
 var file_base_name = get_file_base_name(file_path)
 
 func test_say_ui():
-	var say_panel := add_panel_from_scene(say_panel_scene)
+	var say_panel := add_from_scene(say_panel_scene) as DialoguePanel
 	assert_dialogue_panel(say_panel)
 	watch_rakugo_signals()
 	await wait_parse_and_execute_script(file_path)
+
 	await wait_say({}, "Hello, world !")
 	assert_dialogue_panel_text(say_panel, "", "Hello, world !")
-	# assert_visblity(say_panel)
-
-	assert_do_step()
-	# say_panel.next_btn.pressed.emit()
-	# assert_visblity(say_panel, false)
+	await wait_visblity(say_panel)
+	say_panel.next_btn.pressed.emit()
 	
 	await wait_say({"name": "Sylvie"}, "Hello !")
 	assert_dialogue_panel_text(say_panel, "# Sylvie\n", "Hello !")
-	# assert_visblity(say_panel)
-	
-	assert_do_step()
-	# say_panel.next_btn.pressed.emit()
-	# assert_visblity(say_panel, false)
+	await wait_visblity(say_panel)
+	say_panel.next_btn.pressed.emit()
 
 	await wait_say({}, "My name is Sylvie")
 	assert_dialogue_panel_text(say_panel, "", "My name is Sylvie")
-	# assert_visblity(say_panel)
-
-	assert_do_step()
-	# say_panel.next_btn.pressed.emit()
-	# assert_visblity(say_panel, false)
+	await wait_visblity(say_panel)
+	say_panel.next_btn.pressed.emit()
 
 	await wait_say({}, "I am 18")
 	assert_dialogue_panel_text(say_panel, "", "I am 18")
-	# assert_visblity(say_panel)
+	await wait_visblity(say_panel)
 	
-	assert_do_step()
-	# say_panel.next_btn.pressed.emit()
-	# assert_visblity(say_panel, false)
+	say_panel.next_btn.pressed.emit()
+	await wait_visblity(say_panel, false)
 
 	await wait_execute_script_finished(file_base_name)
 
